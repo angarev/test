34eef89f38aaa2514aa3515c41aabc4304731720
preslyp <pancho.angarev@gmail.com>
2020-10-14 11:24:04 +0300

    test again

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import Nav from '../src/components/nav';
 
-export default function IndexPage() {
+function IndexPage() {
 	return (
 		<div>
 			<Nav />
 			<div className='py-20'>
-				<h1 className='text-5xl text-center text-accent-1'>
-					Next.js + Tailwind CSS
-				</h1>
+				<h1 className='test'>Next.js + Tailwind CSS</h1>
+				<style jsx>{`
+					h1.test {
+						@apply bg-green-500 text-5xl text-center text-accent-1;
+					}
+				`}</style>
 			</div>
 		</div>
 	);
 }
+
+export default IndexPage;
